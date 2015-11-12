4fdd219096721750b84872bcd48e31965115b360
Pedro Guridi <pguridi@onapsis.com>
2015-11-12 14:52:47 -0300

    fixed player connect timeout

--- turnboxed/basebot.py
+++ turnboxed/basebot.py
@@ -39,4 +39,15 @@ class BaseBot(object):
 
             self._get_turns()
 
-
+if __name__ == "__main__":
+    import script
+    from script import *
+    with open(script.__file__, 'r') as f:
+        script_content = f.read()
+    cs = re.findall('class\ (.*?)\(GameBot', script_content)
+    if len(cs) > 0:
+        klass = globals()[cs[-1]]
+        bot_instance = klass()
+        bot_instance._get_turns()
+    else:
+        raise Exception("No valid bot found")
